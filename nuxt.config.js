f9daebb97cd94dff5483879007ec4da5904ace5a
Luis Gago <luisgago@lagc-peru.com>
2021-04-03 22:43:06 -0500

    linter | resolved problem of new elements

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,7 +44,7 @@ export default {
     '@nuxtjs/axios',
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
-    '@nuxtjs/strapi',
+    '@nuxtjs/strapi'
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
