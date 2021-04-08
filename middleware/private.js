f541018706c4df832998951d8fe6693c8bbbc162
Luis Gago <luisgago@lagc-peru.com>
2021-04-08 14:14:11 -0500

    remote auth for store

--- middleware/private.js
+++ middleware/private.js
@@ -1,8 +1,5 @@
-export default function ({ store, redirect }) {
-  const auth = store.getters['auth/getAuth']
-  console.log('auth1', auth)
-  console.log('auth2', auth.email)
-  if (auth.email === undefined) {
+export default function ({ $strapi, redirect }) {
+  if (!$strapi.user) {
     redirect('/')
   }
 }
